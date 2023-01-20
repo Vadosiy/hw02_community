f7fb3da88019118af67ccb022f29cd5e8ca39f65
Vadosiy <dubrovskyvadim97@gmail.com>
2023-01-21 00:47:09 +0300

    создание приложения yatube

---
+++ yatube/posts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'posts'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('group/<slug:slug>/',
+         views.group_posts,
+         name='group_list'),
+    path(
+        'posts/<int:pk>/',
+        views.post_detail,
+        name='post_detail'
+    ),
+]
